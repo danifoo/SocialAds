8423aed1f214af5706a47270b9eaf44bacdecb6f
Dani <danifooladi@gmail.com>
2024-05-03 00:52:35 +0100

    added commented attach/detach

--- src/socialads2021/Advertiser.java
+++ src/socialads2021/Advertiser.java
@@ -217,7 +217,18 @@ public class Advertiser implements Subject{
     public boolean notify(Advertisement advertisement) {
     	Regulator regulator =  Regulator.getInstance();
     	return regulator.update(advertisement);
-    }    
+    }  
     
+    /**
+    @Override
+	public void attach(Observer observer){
+		observers.add(observer);
+    }
+    
+   	@Override
+	public void detach(Observer observer){
+		observers.remove(observer);
+	}
+	**/
 }
 
--- src/socialads2021/Subject.java
+++ src/socialads2021/Subject.java
@@ -2,4 +2,12 @@ package socialads2021;
 
 public interface Subject {
 	public boolean notify(Advertisement advertisement);
+	
+	/**
+	 * The attach and detach methods have been left in the code as a comment
+	 * to show that they had previously been implemented but removed later as they server no practical purpose. 
+	 */
+	
+	//public void attach(Observer observer);
+	//public void detach(Observer observer);
 }
